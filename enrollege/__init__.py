9d3a9f0886920001c82947cf3fb3628b056a25fa
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-01 15:17:52 +0100

    feat: configure and instantiate sqlalchemy

--- enrollege/__init__.py
+++ enrollege/__init__.py
@@ -1,4 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
 app.app_context().push()
+
+# SQLAlchemy configuration
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+db_name = 'enrollege.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, db_name)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# SQLAlchemy instantiation
+db = SQLAlchemy(app)
