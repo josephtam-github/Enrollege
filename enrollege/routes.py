068622a43e70fb37429b3c812156baf490f4819a
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-04 00:34:55 +0100

    feat: create routes rendering filler html

--- enrollege/routes.py
+++ enrollege/routes.py
@@ -8,3 +8,26 @@ from flask_login import login_user, logout_user, login_required, current_user
 def home_page():
     return render_template('index.html')
 
+@app.route('/login')
+def logout_page():
+    return render_template('account.html')
+
+
+@app.route('/profile')
+def profile_page():
+    return render_template('profile.html')
+
+
+@app.route('/result')
+def result_page():
+    return render_template('result.html')
+
+
+@app.route('/settings')
+def settings_page():
+    return render_template('settings.html')
+
+
+@app.route('/rankings')
+def rankings_page():
+    return render_template('rankings.html')
