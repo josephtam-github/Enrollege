dc48cb1b0b49cc4e3ded5a53bfdef12f84f00e72
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-04 00:51:00 +0100

    feat: add login route

--- enrollege/routes.py
+++ enrollege/routes.py
@@ -2,14 +2,26 @@ from enrollege import app
 from enrollege.models import Users
 from flask import render_template
 from flask_login import login_user, logout_user, login_required, current_user
+from enrollege.forms import RegisterForm
 
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
+
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html')
+
+
 @app.route('/login')
-def logout_page():
+def login_page():
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
     return render_template('account.html')
 
 
