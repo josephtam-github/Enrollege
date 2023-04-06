81050b48ec6534ef58a9f3a1e821556d94179e59
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-06 23:28:48 +0100

    feat: add crud methods to Users db model

--- enrollege/models.py
+++ enrollege/models.py
@@ -30,3 +30,42 @@ class Users(db.Model, UserMixin):
 
     def check_password_correctness(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=30), nullable=False, unique=True)
+    firstname = db.Column(db.String(length=30), nullable=False)
+    lastname = db.Column(db.String(length=30), nullable=False)
+    password_hash = db.Column(db.String(length=60), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correctness(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        return cls.query.get_or_404(user_id)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
