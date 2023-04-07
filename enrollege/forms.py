da9d2124d2bade5c106a8719d93d635d87ab39e5
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-07 10:55:10 +0100

    feat: create profile form

--- enrollege/forms.py
+++ enrollege/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, IntegerRangeField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError, NumberRange
 from enrollege.models import Users
 import re
 
@@ -33,11 +33,17 @@ class RegisterForm(FlaskForm):
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     confpassword = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), DataRequired()])
-    submit = SubmitField(label='Sign up')
+    submit = SubmitField(label='Create Account')
 
 
 class LoginForm(FlaskForm):
     username = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     submit = SubmitField(label='Sign in')
+
+
+class ProfileForm(FlaskForm):
+    sat_score = IntegerField(label='Sat Score:', validators=[NumberRange(min=400, max=1600), DataRequired()])
+    tuition = IntegerRangeField(label='Tuition Range:', validators=[NumberRange(min=1, max=1000000), DataRequired()])
+    submit = SubmitField(label='Update Profile')
 
