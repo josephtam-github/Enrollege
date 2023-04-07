2e0af5e6199ad6db812e4cc04ee111ce7b70febc
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-07 10:35:43 +0100

    style: fix typo of db.Column

--- enrollege/models.py
+++ enrollege/models.py
@@ -51,8 +51,8 @@ class Profile(db.Model, UserMixin):
     __tablename__ = "profile"
     id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer(), db.ForeignKey("users.id"), unique=True, nullable=False)
-    sat_score = db.column(db.Integer(), nullable=False)
-    max_tuition = db.column(db.Integer(), nullable=False)
+    sat_score = db.Column(db.Integer(), nullable=False)
+    max_tuition = db.Column(db.Integer(), nullable=False)
 
     def __repr__(self):
         return f'SAT: {self.sat_score}, Max Tuition: {self.max_tuition}'
