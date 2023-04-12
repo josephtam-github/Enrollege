343aec0829e43c91083b8e6e3937b709d62f7f66
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-12 01:43:50 +0100

    feat: create function to scrape university name and ranking from website

--- enrollege/console/processor/scraper.py
+++ enrollege/console/processor/scraper.py
@@ -1,3 +1,4 @@
+import urllib.request
 import csv
 
 
@@ -15,3 +16,24 @@ def get_ranking_field(file, column_names):
     reader.close()
     return result
 
+
+def get_uni_ranking():
+    """Scrapes https://www.4icu.org/us/ for a ranked list of universities"""
+    url = "https://www.4icu.org/us/"
+    handle = urllib.request.urlopen(url)
+    html = handle.read()
+    html = html.decode("utf8")
+    result = {}
+    location = 0
+    rank = 0
+    while True:
+        try:
+            location1 = html.index('.htm">', location)
+            location2 = html.index('</a>', location1)
+            result[html[(location1 + len('.htm">')): location2]] = rank
+            rank = rank + 1
+            location = location2 + 1
+        except ValueError as ex:
+            break
+
+    return result
