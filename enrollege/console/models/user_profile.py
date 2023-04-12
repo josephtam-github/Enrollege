3db57ec5d7acc60f8182141152ccce9141f5afd0
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-12 01:31:24 +0100

    feat: create user input class model for recommendation engine

---
+++ enrollege/console/models/user_profile.py
@@ -0,0 +1,7 @@
+class UserProfile:
+    sat_score = 0
+    max_tuition = 0
+
+    def __init__(self, sat_score, max_tuition):
+        self.sat_score = sat_score
+        self.max_tuition = max_tuition
