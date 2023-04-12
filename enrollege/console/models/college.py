415440dcbd45044f6aff8fa8f3e9f0afd7e1b85c
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-04-12 01:32:59 +0100

    feat: create college class model methods

--- enrollege/console/models/college.py
+++ enrollege/console/models/college.py
@@ -17,3 +17,10 @@ class CollegeInfo:
         self.accept_rate = accept_rate
         self.male_ratio = male_ratio
 
+    def to_string(self):
+        return self.city + '\t' + self.state + '\t' + str(self.rank) + '\t' + str(self.tuition) + '\t' + str(self.sat) \
+               + '\t' + str(self.accept_rate) + '\t' + '\t' + str(self.male_ratio)
+
+    def to_string_with_name(self):
+        return 'city:' + self.city + '\tstate:' + self.state + '\trank:' + str(self.rank) + '\ttuition:' + str(
+            self.tuition) + '\tsat:' + str(self.sat) + '\tAC:' + str(self.accept_rate) + '\tMal:' + str(self.male_ratio)
